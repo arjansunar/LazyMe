d2934592d4d43d7e9470aaad599465c342bff3de
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-04-09 11:10:48 +0545

    updating script

--- setup.sh
+++ setup.sh
@@ -6,6 +6,12 @@ sudo apt update && sudo apt upgrade -y && sudo apt install git -y
 # install ansible
 sudo apt install ansible
 
+# install community general collection
+ansible-galaxy collection install community.general
+
+# remove if previously cloned
+rm -rf /tmp/rjan-setup
+
 # clone the playbook to /tmp and run ansible-playbook
 git clone https://github.com/arjansunar/ansible-setup.git /tmp/rjan-setup
 
