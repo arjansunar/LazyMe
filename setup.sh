8000ee0e31d306d9a1cee82aa49b7880ee7bf534
arjansunar <arjan.gahatrajsunar@gmail.com>
2023-08-30 14:53:48 +0545

    feat: use single file to include all playbooks

--- setup.sh
+++ setup.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/bash
 
-# update and install git 
-sudo apt update && sudo apt upgrade -y && sudo apt install git -y
-
 # install ansible
 sudo apt install ansible
 
-# install community general collection
-# ansible-galaxy collection install community.general
-
 # remove if previously cloned
-rm -rf /tmp/rjan-setup
+rm -rf /tmp/dev-setup
 
 # clone the playbook to /tmp and run ansible-playbook
-git clone https://github.com/arjansunar/ansible-setup.git /tmp/rjan-setup
+git clone https://github.com/arjansunar/ansible-setup.git /tmp/dev-setup
 
 # run ansible playbook
-ansible-playbook /tmp/rjan-setup/local.yml --ask-become-pass
+ansible-playbook /tmp/rjan-setup/setup.yml --ask-become-pass
